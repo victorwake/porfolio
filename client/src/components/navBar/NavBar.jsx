5861fcae09608cda6425c800fdceefb270813550
Victor Pinto <victorpintowake@gmail.com>
2023-03-17 17:30:23 -0300

    arreglos

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,14 +1,8 @@
 import './navBar.css';
 import home from '../../image/home.png';
-import React, { useState } from 'react';
+// import logo2 from '../../image/logo2.png';
 
 export function NavBar () {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-  
     return (
         <div className="navBar" id="home">
             <header class="image-bg1" data-parallax="scroll" data-image-src="http://img03.deviantart.net/5b62/i/2016/333/b/5/img_1242bis_by_trin003-daq0py8.jpg">
@@ -29,7 +23,7 @@ export function NavBar () {
               </a>
             </div>
               
-            <div class="collapse navbar-collapse" id="myNavbar">
+            <div class="collapse navbar-collapse pull-right" id="myNavbar">
                 <ul class="nav navbar-nav">
                   
                     <li class="hidden"><a class="page-scroll" href="#home"></a></li>
@@ -39,7 +33,7 @@ export function NavBar () {
                     <li><a class="page-scroll" href="#cv">Courses</a></li>
                     <li><a class="page-scroll" href="#contact">Contact</a></li>
                 </ul>
-                <div class="social-network ">
+                <div class="social-network pull-right">
                 <a href="https://www.linkedin.com/in/victor-pinto-fullstack/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
                 <a href="https://www.facebook.com/Victorpintowake" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a> 
                 <a href="https://www.instagram.com/victorwake/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a> 
